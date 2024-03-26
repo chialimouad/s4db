7a32ac7a2dbf428c9883aa27556c3eb6296e3845
chialimouad <159821581+chialimouad@users.noreply.github.com>
2024-03-26 16:04:59 +0100

    Update controller.js

--- controller/controller.js
+++ controller/controller.js
@@ -2,8 +2,8 @@ const userserv =require('../services/services')
 
 
 exports.register= async(req,res,next)=>{try{
-    const {email,fullname,phonenumber,idpulse,willaya,password,Age,Grp}=req.body
-    const usercontrol =await userserv.registeruser(email,fullname,phonenumber,idpulse,willaya,password,Age,Grp)
+    const {email,fullname,phonenumber,idpulse,willaya,password,Age,Grp,maladie}=req.body
+    const usercontrol =await userserv.registeruser(email,fullname,phonenumber,idpulse,willaya,password,Age,Grp,maladie)
 
     res.json({status:true,success:"user succsefully"})
     
