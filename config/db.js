9c856bd066b6cb7fa475ad3ebd97312be45a9304
chialimouad <159821581+chialimouad@users.noreply.github.com>
2024-03-26 17:28:02 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose')
-const connection = mongoose.createConnection('mongodb+srv://mouad:mouadchiali@projets4.o9xgxx9.mongodb.net/Patients').on('open',()=>{
+const connection = mongoose.createConnection('mongodb+srv://Mouad:mouadchiali@projets4.o9xgxx9.mongodb.net/Patients').on('open',()=>{
     console.log("connected")
 }).on('error',()=>{
     console.log("not connected")
