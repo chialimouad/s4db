8998898996372c3fdf1ff1060f283bda7a9eddbf
Chiali Mouad <chialimouad22314@gmail.com>
2024-03-27 17:27:38 +0100

    newdoc

--- services/doctorservice.js
+++ services/doctorservice.js
@@ -1,9 +1,9 @@
 const dbqa=require('../models/doctormodel')
 
 class servicedoctor{
-    static async registerdoctor(email,fullname,phonenumber,specialite,willaya,age,password){
+    static async registerdoctor(email,fullname,phonenumber,specialite,willaya,Age,password){
       try{
-      const par=new dbqa({email,fullname,phonenumber,specialite,willaya,age,password})
+      const par=new dbqa({email,fullname,phonenumber,specialite,willaya,Age,password})
       return await par.save()
 
       }catch(err){console.log(err)}
