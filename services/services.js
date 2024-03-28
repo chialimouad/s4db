d82669915966539e6899a5fc12f11c625ea305c3
Chiali Mouad <chialimouad22314@gmail.com>
2024-03-28 23:59:16 +0100

    NEWW

--- services/services.js
+++ services/services.js
@@ -1,8 +1,8 @@
 const dbq=require('../models/models')
 class serviceuser{
-     static async registeruser(email,fullname,phonenumber,idpulse,willaya,password,Age,Grp,maladie){
+     static async registeruser(userId,email,fullname,phonenumber,idpulse,willaya,password,Age,Grp,maladie){
        try{
-       const par=new dbq({email,fullname,phonenumber,idpulse,willaya,password,Age,Grp,maladie})
+       const par=new dbq({userId,email,fullname,phonenumber,idpulse,willaya,password,Age,Grp,maladie})
        return await par.save()
           
        }catch(err){console.log(err)}
