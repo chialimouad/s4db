cbc35de84b22458f656758c2312c2aaf632b8e02
Chiali Mouad <chialimouad22314@gmail.com>
2024-04-02 23:09:36 +0200

    iji

--- models/models.js
+++ models/models.js
@@ -1,46 +1,42 @@
+
+const usershema = require('../models/doctormodel')
 const mongoose=require('mongoose')
 const db=require('../config/db')
-const usershema = require('../models/doctormodel')
 const { Schema }=mongoose
-
 const userschema =new Schema({
-   
-// userId:{
-//    type:Schema.Types.ObjectId,
-//    ref:usershema.modelName,
-// },
-email :{
-   type:String,
-   required:true,
-   unique:true,
- 
-},
-fullname :{
-   type:String,
-   required:true,
-  
-},
-phonenumber:{
-  type:Number,
-  required:true,
-  unique:true,
-},
-maladie:{
-   type:String,
-   required:true,
-},
-willaya:{
-   type:String,
-   required:true,
-},
-Age:{
-   type:Number,
-   required:true,
-},
-password:{
-   type:String,
-   required:true,
-},
+    email :{
+        type:String,
+        required:true,
+        unique:true,
+      
+    },
+    fullname :{
+        type:String,
+        required:true,
+       
+    },
+    phonenumber:{
+       type:Number,
+       required:true,
+       unique:true,
+    },
+   //  maladie:{
+   //      type:String,
+   //      required:true,
+   //   },
+     willaya:{
+        type:String,
+        required:true,
+     },
+     Age:{
+        type:Number,
+        required:true,
+     },
+     password:{
+        type:String,
+        required:true,
+     },
+     
 
 
 
@@ -49,3 +45,4 @@ password:{
 
 const Patients=db.model('Patients',userschema)
 module.exports=Patients
+
