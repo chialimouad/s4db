e08743ff0c484f0ec524f35d6fdc7adedc618783
Chiali Mouad <chialimouad22314@gmail.com>
2024-04-07 16:44:57 +0200

    kjhkj

--- routes/routes.js
+++ routes/routes.js
@@ -2,21 +2,22 @@ const route=require('express').Router()
 const dbcontrol=require('../controller/controller')
 const dbq=require('../models/models')
 const dbadvie=require('../models/advice')
-const dbqdoc=require('../models/doctormodel')
 const multer  = require('multer');
+const dbqdoc=require('../models/doctormodel')
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 const { deleteAdvice } = require('../controller/deleteadvice');
 route.post('/registeruser',dbcontrol.registeruser)
-route.post('/registerdoctor',dbcontrol.registerdoctor)
+route.post('/registerdoctor',upload.single('photo'),dbcontrol.registerdoctor)
 route.post('/logindoc',dbcontrol.logindoc)
 route.post('/loginuser',dbcontrol.loginuser)
 route.post('/getuser',dbcontrol.getdatacontroller)
 route.post('/delete',dbcontrol.deleteuser)
 route.post('/advice',dbcontrol.registeradvice)
 route.post('/data', dbcontrol.dbloadd)
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
 
-route.post('/upload', upload.single('photo'), dbcontrol.newload);
+
+//route.post('/upload', upload.single('photo'), dbcontrol.newload);
 
 //route.post('/update', dbcontrol.updateuser)
 route.get('/fetch',async (req,res)=>{
