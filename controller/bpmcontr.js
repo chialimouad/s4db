8b236f6710128c88856adf7cdc76a1257f548b7a
Chiali Mouad <chialimouad22314@gmail.com>
2024-04-18 00:48:45 +0200

    jk

--- controller/bpmcontr.js
+++ controller/bpmcontr.js
@@ -3,7 +3,7 @@ const HeartbeatModel = require('../models/bpm');
 
 class HeartbeatController {
   constructor() {
-    this.heartbeatModel = new HeartbeatModel('mongodb+srv://mouadchiali:mouadchiali@clustertestprojet.n7r4egf.mongodb.net/', 'doctors', 'heartbeats');
+    this.heartbeatModel = new HeartbeatModel('mongodb+srv://mouadchiali:mouadchiali@clustertestprojet.n7r4egf.mongodb.net/', 'Hbpm', 'heartbeats');
   }
 
   async postHeartbeat(req, res) {
