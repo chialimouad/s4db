e026fb589da73261fca1463f44666ba00dd9853a
Chiali Mouad <chialimouad22314@gmail.com>
2024-04-17 22:28:16 +0200

    uhu

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
-const app=require('./index')
+// app.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const userRoutes = require('./routes/routes');
+const http = require('http');
+const WebSocket = require('ws');
+const controller = require('./controller/bpmcontr');
 
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 
-const userfecth = require('./controller/controller')
+// WebSocket event handler
+wss.on('connection', controller.handleWebSocketConnection);
 
-var port = process.env.PORT || 5555 
-app.get('/1',(req,res)=>{
-    res.send("userfecth.fetch")
-})
-app.listen(3000,()=>{
-    console.log("start listening port 3000")
-})
+app.use(bodyParser.json());
+app.use(cors({ origin: '*' }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/12', userRoutes);
+
+module.exports = server;
