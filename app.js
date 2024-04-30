d2b6f83a7633242f94652464403fe689a965c00f
Chiali Mouad <chialimouad22314@gmail.com>
2024-04-30 12:42:04 +0200

    iui

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const userRoutes = require('./routes/routes');
-const patientDataRoutes = require('./routes/routebpm');
+
+
+
 
 const PORT = process.env.PORT ||3500;
 
@@ -12,7 +14,7 @@ const PORT = process.env.PORT ||3500;
 app.use(bodyParser.json());
 app.use(cors({ origin: '*' }));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/data', patientDataRoutes);
+
 app.use('/12', userRoutes);
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
